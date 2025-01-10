6d1265e4340e270171cc9d087e900f691102f0fe
Sahil08 <sahilnaik2150@gmail.com>
2025-01-10 22:56:13 +0530

    added validation for admin's sc add function

--- car_care_backend/controllers/adminController.js
+++ car_care_backend/controllers/adminController.js
@@ -1,4 +1,5 @@
-
+import validator from "validator"
+import bcrypt from "bcrypt"
 
 
 
@@ -12,7 +13,35 @@ const addServCenter = async () => {
 
     const imageFile = req.file 
 
-    console.log({sc_name, sc_email, password, serviceType, city, state, image})
+    //checking for all data to add service center (validation)
+    if (!sc_name || !sc_email || !password || !serviceType || !city || !state ) {
+
+      return res.json({success:false, messgae:"Missing details"})
+
+    }
+
+    //validating email format
+    if (!validator.isEmail(sc_email)) {
+      return res.json({success:false, messgae:"Enter a valid email!"})
+    }
+
+    //validating strong password
+    if (password.length < 8) {
+      return res.json({success:false, messgae:"Please enter a strong password!"})
+    }
+
+    //storing password in encrypted form, in the databse using the bcrypt package using salt (hash)
+
+    //hashing sc password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //uploading image to cloudinary
+    //to be completed
+
+    
+
+
 
   } catch (error) {
 
